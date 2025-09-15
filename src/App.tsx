60a7358eddafdc2a6513145293c32dd6610dedef
Lei Xu <leixu@leixu-ubu-sig-vm102>
2025-09-15 13:10:58 +0000

    Optimize document loading logic and update project files

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/docs" element={<Documents />} />
+        <Route path="/docs/*" element={<Documents />} />
       </Routes>
     </div>
   );
